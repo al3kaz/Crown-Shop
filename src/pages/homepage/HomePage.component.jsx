cb0761a29fbd43f221dcea6b71ef631e41018fc5
Aleksander Kazmierczak <aleksander.kazmierczak@gmail.com>
2020-09-27 16:22:31 +0200

    first commit

---
+++ src/pages/homepage/HomePage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Directory from '../../components/directory/directory.component';
+import './HomePage.style.scss'
+
+const HomePage = () => {
+   return (
+      <div className="homepage">
+         <Directory />
+      </div>
+   );
+}
+
+export default HomePage;
