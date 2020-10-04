ddca4572b7a382602a8e1281251c44f4c71170c7
Aleksander Kazmierczak <aleksander.kazmierczak@gmail.com>
2020-10-04 22:13:04 +0200

    finish sgn in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
               id: snapShot.id,
               ...snapShot.data()
             }
-          }, () => { console.log(this.state); }
+          },
           )
 
         })
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import FormInput from '../form-input/form-input.component'
 
-import { signInWithGoogle } from '../../firebase/firebase.utils'
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils'
 
 import CustomButton from '../custom-button/custom-button.component'
 
@@ -17,14 +17,21 @@ class SignIn extends Component {
       }
    }
 
-   handleSubmit = event => {
+   handleSubmit = async event => {
       event.preventDefault()
 
-      this.setState({
-         email: "",
-         password: "",
-      })
-   }
+      const { email, password } = this.state;
+
+      try {
+         await auth.signInWithEmailAndPassword(email, password);
+         this.setState({
+            email: "",
+            password: "",
+         });
+      } catch (error) {
+         console.error(error);
+      }
+   };
 
    handleChange = event => {
       const { value, name } = event.target;
