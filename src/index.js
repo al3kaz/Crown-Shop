90bb41815d2a53ddb9d12b573361653f634dead9
Aleksander Kazmierczak <aleksander.kazmierczak@gmail.com>
2020-11-21 17:21:38 +0100

    serviceWorker

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import {store, persistor} from './redux/store'
+import {store, persistor} from './redux/store';
+import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 import App from './App';
@@ -20,3 +21,5 @@ ReactDOM.render(
 
   document.getElementById('root')
 );
+
+serviceWorker.register();
